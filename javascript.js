1b2785bff8fadfd6c2cdd311ab272c3f5f205655
James Crane <james@cranesnest.us>
2017-02-10 20:11:53 -0600

    Added UI builder

--- javascript.js
+++ javascript.js
@@ -1,14 +1,31 @@
-function Team(number, sct_score) {
+function Team(number, sct_score, score, test) {
     this.number = number;
-    this.sct_score = sct_score || 5;
+    this.sct_score = sct_score;
+    this.score = score;
+    this.test = test;
 }
-function setUI(teamObj) {
-    //CREATE UI
+function setUI(obj) {
+    if (obj == undefined) {
+        obj = new Team();
+    }
+    for (var key in obj) {
+        if (key != Object.keys(obj)[0]) {
+            if (team.hasOwnProperty(key)) {
+                if (!$("#" + key).length) {
+                    //CREATE NEW UI
+                    var elmt = $("<span class='lable'>"+key+": </span><input id='"+key+"'  class='field' type='number'><br/>");
+                    $("#teamInfoForm").append(elmt);
+                }
+                //REFRESH UI
+                $("#"+key).val(team[key]);
+            }
+        }
+    }
 }
 function setTeamInfo () {
-    var foo = $("#teamNumber").val();
+    var teamNumber = $("#number").val();
     $.ajax({
-        url : 'php/setTeamInfo.php?q=' + foo,
+        url : 'php/setTeamInfo.php?q=' + teamNumber,
         type : 'POST',
         data : {data : JSON.stringify(team)}
     }).fail(function(){
@@ -18,20 +35,24 @@ function setTeamInfo () {
     });
 }
 function getTeamInfo () {
-    var foo = $("#teamNumber").val();
+    var teamNumber = $("#number").val();
     $.ajax({
-        url : 'php/getTeamInfo.php?q=' + foo,
+        url : 'php/getTeamInfo.php?q=' + teamNumber,
         type : 'GET',
         dataType : "json",
     }).done(function(data){
-        alert(JSON.stringify(data) + "\n" + status);
-        document.getElementById("header").innerHTML = JSON.stringify(data);
-        //$("#teamNumber").val(data.number);  
+        if (data.number == undefined) {
+            alert("Team does not exist");
+        } else {
+            team = data;
+            setUI(team);
+        }
     });
 }
-//alert(JSON.stringify(dataStore));
-var team = new Team(4931, 10);
+
+var team = new Team(4931);
+alert(JSON.stringify(team));
 
 function setHeader() {
     document.getElementById("header").innerHTML = "Hello World";
-};
+}
