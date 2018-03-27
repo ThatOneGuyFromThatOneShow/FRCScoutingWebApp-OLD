0c7cafd48025d3f89a42acb7fa458f27e116e85c
James Crane <james@cranesnest.us>
2018-03-27 11:10:19 -0500

    Update repairServerData.py

--- python scripts/repairServerData.py
+++ python scripts/repairServerData.py
@@ -38,7 +38,7 @@ for i in data:
 
     inc = 0
     for j in i["Matches__match"]:
-        matchNumber = inc + 
+        matchNumber = inc + 1
         i["Matches__match"][inc]["Match_Number__num"] = matchNumber
         inc += 1
 
